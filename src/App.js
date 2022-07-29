7bf53579880cd53c577b28c9ec40d1cf94185af2
jmh0434 <80581335+jmh0434@users.noreply.github.com>
2022-07-29 16:06:13 +0900

    컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+function Header(){
+  return <header>
+         <h1><a href = "/">Hello World</a></h1>
+       </header>
+}
 
+function Nav(){
+  return <nav>
+         <ol>
+            <li><a href = "/list/1">html</a></li>
+            <li><a href = "/list/2">css</a></li>
+            <li><a href = "/list/3">javascript</a></li>
+         </ol>
+       </nav>
+}
+
+function Article() {
+  return <article>
+          <h2>Welcome</h2>
+            This is..
+         </article>
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header></Header>
+      <Nav></Nav>
+      <Article></Article>
     </div>
   );
 }
