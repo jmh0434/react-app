9d3761f54ea1b9036ed321e856091f28e3684afb
jmh0434 <80581335+jmh0434@users.noreply.github.com>
2022-07-29 20:52:48 +0900

    state-1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import { useState } from "react";
+
 const Header = (props) => {
   return <header>
          <h1><a href = "/" onClick = {(event) => {
@@ -12,7 +14,7 @@ const Nav = (props) => {
   const lis = [];
   for(let i = 0; i < props.topics.length; i++) {
     let t = props.topics[i];
-    lis.push(<li key = {t.id}><a title = {t.title} href = {"/list/"+t.id}
+    lis.push(<li key = {t.id}><a id= {t.id} href = {"/list/"+t.id}
       onClick = {(event) => {
         event.preventDefault();
         props.onChangeMode(event.target.title);
@@ -32,20 +34,42 @@ const Article = (props) => {
          </article>
 }
 const App = () => {
+  const [mode, setMode] = useState('WELCOME');
+  const [id, setId] = useState(null);
   const topics = [
     {id : 1, title : "html", body : "html is.."},
     {id : 2, title : "css", body : "css is.."},
     {id : 3, title : "javascipt", body : "javascript is.."}
   ];
+
+  let content = null;
+  if (mode === 'WELCOME') {
+    content = <Article title = 'WELCOME' body = "Hello React"></Article>
+  } else if (mode === 'READ') {
+      let title, body = null;
+      for (let i = 0; i < topics.length; i++) {
+        if(topics[i].id === id){
+          title = topics[i].title;
+          body = topics[i].body;
+        }
+      }
+      content = <Article title = {title} body = {body}></Article>
+}
   return (
     <div>
-      <Header title = "Hello React" onChangeMode = {()=>{
-        alert('header');
-        }}></Header>
-      <Nav topics = {topics} onChangeMode = {(title)=>{
-        alert(title);
-      }}></Nav>
-      <Article title = "Welcome" body = "Web"></Article>
+      <Header
+        title="Hello React"
+        onChangeMode={() => {
+          setMode("WELCOME");
+        }}
+      ></Header>
+      <Nav
+        topics={topics}
+        onChangeMode={(_id) => {
+          setMode("READ");
+          setId(_id);
+        }}></Nav>
+      {content}
     </div>
   );
 }
